132b51bbbfecc5d550a22187cc90ce27c76f3f8b
Jacob Chaney <jacobchaneyschool@gmail.com>
2025-04-01 13:42:28 -0500

    Completed show method for file_storage controller

--- app/controllers/file_storage_controller.rb
+++ app/controllers/file_storage_controller.rb
@@ -1,12 +1,16 @@
 class FileStorageController < ApplicationController
 
   def index
-
   end
 
   def show
     @file = params[:file]
+    file_path = Rails.root.join('public', file)
+
+    if File.exist?(file_path)
+      send_file file_path
+    else
+      render plain: "ERROR: File is not available", status: not_found
+    end
   end
-
-
 end

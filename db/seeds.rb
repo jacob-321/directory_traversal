c284124c9a270d23358e7f296b71c5b93a0d1159
Jacob Chaney <jacobchaneyschool@gmail.com>
2025-04-11 18:50:16 -0500

    More seed files were added

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,7 +30,9 @@ File.write(storage_dir.join("reminder.txt"), "REMINDER: Archive passwords and in
 File.write(storage_dir.join("notes(2).txt"), "Notes for various information. Previous notes have been moved to secrets.")
 File.write(storage_dir.join("asdf.txt"), "REMINDER:\n
 The secrets directory has been moved out of the file_storage directory for organization purposes.")
-
+File.write(storage_dir.join("random.txt"), "REMINDER: remove gmail txt file in secrets directory")
+File.write(storage_dir.join("diary.txt"), "REMINDER: remove diary txt file from secrets directory")
+File.write(storage_dir.join("erpghsfgo.txt"), "Be sure to move card info into secrets directory before making the website public!")
 
 
 # Files for "secrets" directory.
@@ -47,3 +49,7 @@ File.write(secrets_dir.join("info.txt"), "Social Security Number: 123 45 6789\n
 Home Address: 1234 Directory Street")
 File.write(secrets_dir.join("notes.txt"), "Be sure to remove 'secrets' directory so nobody can access confidential files.")
 File.write(secrets_dir.join("notes(1).txt"), "My UofM username is 'User' and my password is 'qwerty123'")
+File.write(secrets_dir.join("gmail.txt"), "My Gmail account is 12345example and my password is qwertyuiop123")
+File.write(secrets_dir.join("dairy.txt"), "02/11/2025:\nToday I studied for my midterm exams. I hope I did well on them!
+\n03/23/2025:\nI got my grades back for my midterm exams. I did better than I expected!\n04/01/2025:\nI can't wait until summer break!")
+File.write(secrets_dir.join("cardinfo.txt"), "My credit card number is 1234 5678 9010\nMy debit card number is 0109 8765 4321")
